49770748da98169989edcacde4f1d8805830bad4
QuangLee <quanglee5923@fmail.com>
2023-04-21 13:36:45 +0700

    bt ngay 14-04

---
+++ src/KhaibaoNhanvien.java
@@ -0,0 +1,9 @@
+public class KhaibaoNhanvien {
+public static void main(String[] args) throws Exception{
+        Nhanvien nv = new Nhanvien();
+        nv.thongtinNhanVien();
+        nv.tinhluong();
+        nv.phuongthuchienthinhanvien();
+    }
+}
+
